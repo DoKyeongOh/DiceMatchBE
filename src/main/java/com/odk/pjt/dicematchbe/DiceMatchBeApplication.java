7d6d85e75e91c3be227033a7cf16fcda1cc838f5
DESKTOP-M911MEH\User <dkproh@gmail.com>
2025-03-31 01:01:04 +0900

    feat - add repository bean excludeFilter
    - for only entried annotation of @Repository will register for bean

--- src/main/java/com/odk/pjt/dicematchbe/DiceMatchBeApplication.java
+++ src/main/java/com/odk/pjt/dicematchbe/DiceMatchBeApplication.java
@@ -2,7 +2,15 @@ package com.odk.pjt.dicematchbe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
 
+@EnableJpaRepositories(
+        basePackages = "com.odk.pjt.dicematchbe",
+        includeFilters = { @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = { Repository.class }) }
+)
 @SpringBootApplication
 public class DiceMatchBeApplication {
 
