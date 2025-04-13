63d77851c3c5cd668b6b1a86a94050d8f1bc2915
DoKyeong Oh <dkproh@gmail.com>
2025-04-13 21:21:32 +0900

    refactor - seperate auth, account logic

--- src/main/java/com/odk/pjt/dicematchbe/account/basic/BasicAccountService.java
+++ src/main/java/com/odk/pjt/dicematchbe/account/basic/BasicAccountService.java
@@ -9,6 +9,8 @@ import com.odk.pjt.dicematchbe.util.HashEncryptionUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class BasicAccountService {
 
@@ -19,8 +21,18 @@ public class BasicAccountService {
         this.repository = repository;
     }
 
-    public BasicAccount getBasicAccount(BasicAccountDTO dto) throws DiceMatchException {
-        validateBasicAccountDTO(dto);
+    public Optional<BasicAccount> getBasicAccount(BasicAccountDTO dto) throws DiceMatchException {
+        if (dto == null) {
+            throw new BadEntityInputException("null");
+        }
+
+        if (dto.getIdentity() == null || dto.getIdentity().isEmpty()) {
+            throw new BadEntityInputException("identity");
+        }
+
+        if (dto.getPassword() == null || dto.getPassword().isEmpty()) {
+            throw new BadEntityInputException("password");
+        }
 
         try {
             String passwordHash = HashEncryptionUtil.encrypt("SHA-256", dto.getPassword());
@@ -29,27 +41,26 @@ public class BasicAccountService {
             throw new DiceMatchException("password hashing fail");
         }
 
-        return repository.findByIdentityAndPassword(dto.getIdentity(), dto.getPassword()).orElseThrow(() ->
-                new EntityNotFoundException(""));
+        return repository.findByIdentityAndPassword(dto.getIdentity(), dto.getPassword());
     }
 
     public BasicAccount register(BasicAccountDTO dto) throws DiceMatchException {
-        validateBasicAccountDTO(dto);
+        if (getBasicAccount(dto).isPresent()) {
+            throw new EntityAlreadyExistException("basic account");
+        }
 
         try {
             String passwordHash = HashEncryptionUtil.encrypt("SHA-256", dto.getPassword());
             dto.setPassword(passwordHash);
         } catch (Exception e) {
             throw new DiceMatchException("password hashing fail");
-        }
-
-        if (repository.findByIdentityAndPassword(dto.getIdentity(), dto.getPassword()).isPresent()) {
-            throw new EntityAlreadyExistException("");
         }
 
         BasicAccount basicAccount = new BasicAccount();
         basicAccount.setIdentity(dto.getIdentity());
         basicAccount.setPassword(dto.getPassword());
+        // TODO: 시간도 시스템에서 발급하는걸로 가는게 의미상 더 맞을듯. 크게 의미는 없지만, 발급은 시스템에서 하니까.
+        // 통신시간딜레이
         return repository.save(basicAccount);
     }
 
@@ -68,20 +79,6 @@ public class BasicAccountService {
         basicAccount.setUserId(userId);
 
         return repository.save(basicAccount);
-    }
-
-    private static void validateBasicAccountDTO(BasicAccountDTO dto) throws DiceMatchException {
-        if (dto == null) {
-            throw new BadEntityInputException("null");
-        }
-
-        if (dto.getIdentity() == null || dto.getIdentity().isEmpty()) {
-            throw new BadEntityInputException("identity");
-        }
-
-        if (dto.getPassword() == null || dto.getPassword().isEmpty()) {
-            throw new BadEntityInputException("password");
-        }
     }
 
 }
